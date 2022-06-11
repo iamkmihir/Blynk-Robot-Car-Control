20bd9457bf83785b9946af2ee7aa2d143b5b915c
iamkmihir <85690694+iamkmihir@users.noreply.github.com>
2022-06-11 18:08:57 +0530

    Update Robot_Control.ino

--- Robot_Control.ino
+++ Robot_Control.ino
@@ -1,5 +1,5 @@
-#define BLYNK_TEMPLATE_ID "TMPL0D2T-hLH"
-#define BLYNK_DEVICE_NAME "Robot Control"
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_DEVICE_NAME ""
 #define BLYNK_FIRMWARE_VERSION        "0.1.0"
 #define BLYNK_PRINT Serial
 #define APP_DEBUG
